41982c7d2d750f4293cc724a9e5736c74e7a3e52
nivedita285 <thakur.nivedita008@gmail.com>
2024-03-21 17:57:59 +0530

    faq page added

---
+++ BooksApplication/src/main/reactapp/booksapp/src/components/FAQ.js
@@ -0,0 +1,146 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function FAQ() {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    return (
+        <Container
+            id="faq"
+            sx={{
+                pt: { xs: 4, sm: 12 },
+                pb: { xs: 8, sm: 16 },
+                position: 'relative',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: { xs: 3, sm: 6 },
+            }}
+        >
+            <Typography
+                component="h2"
+                variant="h4"
+                color="text.primary"
+                sx={{
+                    width: { sm: '100%', md: '60%' },
+                    textAlign: { sm: 'left', md: 'center' },
+                }}
+            >
+                Frequently asked questions
+            </Typography>
+            <Box sx={{ width: '100%' }}>
+                <Accordion
+                    expanded={expanded === 'panel1'}
+                    onChange={handleChange('panel1')}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1d-content"
+                        id="panel1d-header"
+                    >
+                        <Typography component="h3" variant="subtitle2">
+                            How do I contact customer support if I have a question or issue?
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography
+                            variant="body2"
+                            gutterBottom
+                            sx={{ maxWidth: { sm: '100%', md: '70%' } }}
+                        >
+                            You can reach our customer support team by emailing
+                            <Link> 21ecuos129@ddu.ac.in </Link>
+                             We&apos;re here to assist you
+                            promptly.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion
+                    expanded={expanded === 'panel2'}
+                    onChange={handleChange('panel2')}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2d-content"
+                        id="panel2d-header"
+                    >
+                        <Typography component="h3" variant="subtitle2">
+                            How can i add new book in database as admin?
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography
+                            variant="body2"
+                            gutterBottom
+                            sx={{ maxWidth: { sm: '100%', md: '70%' }, textAlign:'left' }}
+                        >
+                            After logging in as admin, go to the admin tab and provide all the necessary
+                            details of the book in the add new book section. ID of book is not needed as it will
+                            be auto-generated in database.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion
+                    expanded={expanded === 'panel3'}
+                    onChange={handleChange('panel3')}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel3d-content"
+                        id="panel3d-header"
+                    >
+                        <Typography component="h3" variant="subtitle2">
+                            How can i update an existing book?
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography
+                            variant="body2"
+                            gutterBottom
+                            sx={{ maxWidth: { sm: '100%', md: '70%' }, textAlign:'left'}}
+                        >
+                            You can update a book based on it's unique ID. In the update book section under the admin tab,
+                            provide the correct ID of book and enter the details wihich you want to update.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion
+                    expanded={expanded === 'panel4'}
+                    onChange={handleChange('panel4')}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel4d-content"
+                        id="panel4d-header"
+                    >
+                        <Typography component="h3" variant="subtitle2">
+                            How can i sign up for the website?
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography
+                            variant="body2"
+                            gutterBottom
+                            sx={{ maxWidth: { sm: '100%', md: '70%' }, textAlign:'left' }}
+                        >
+                            You can quite simply provide your email and password to sign up for the website.
+                            Using the same credentials, you can log in as well.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </Box>
+        </Container>
+    );
+}
